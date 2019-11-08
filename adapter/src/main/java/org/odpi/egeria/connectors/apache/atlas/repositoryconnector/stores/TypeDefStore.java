f472cbe2d5176a00d76e9e7eb5ebe10a305fd876
Christopher Grote <chris@thegrotes.net>
2019-11-08 11:15:15 +0000

    Ensures JavaDocs are built
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

--- adapter/src/main/java/org/odpi/egeria/connectors/apache/atlas/repositoryconnector/stores/TypeDefStore.java
+++ adapter/src/main/java/org/odpi/egeria/connectors/apache/atlas/repositoryconnector/stores/TypeDefStore.java
@@ -596,6 +596,7 @@ public class TypeDefStore {
          * Retrieve the corresponding Apache Atlas endpoint given an OMRS endpoint attribute name.
          *
          * @param omrsEndpointName the OMRS endpoint attribute name
+         * @param entityPrefix the prefix (if any) for the entity
          * @return Endpoint
          */
         public Endpoint getMatchingAtlasEndpoint(String omrsEndpointName, String entityPrefix) {
