15803c1f9135824e22a46bfd2a681f8ba0773532
Christopher Grote <chris@thegrotes.net>
2020-12-03 11:08:33 +0000

    Adds package info headings
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

---
+++ apache-atlas-adapter/src/test/java/org/odpi/egeria/connectors/apache/atlas/mocks/package-info.java
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+
+/**
+ * Defines and implements mocked resources for testing the Apache Atlas connector.
+ */
+package org.odpi.egeria.connectors.apache.atlas.mocks;
